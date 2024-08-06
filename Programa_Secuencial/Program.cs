0a9686722efe1c2440dd993a37c9621072e866b1
Rguezpjm <84132936+Rguezpjm@users.noreply.github.com>
2024-08-05 22:48:10 -0400

    Add files via upload
     1) Diseñar un programa que solicite al usuario dos números y, si el primer número es más grande, calcule y muestre su suma y resta. De lo contrario, calculará y mostrará su producto y cociente.
    
    2) Leer tres calificaciones y calcular el promedio. Si el promedio es igual o superior a siete, imprimir un mensaje indicando que el alumno ha aprobado.
    
    3) Desarrollar un programa que calcule el nivel de un postulante a partir de los resultados de un test. El programa recibirá como entrada el número total de preguntas y las respuestas correctas, y devolverá un nivel según el porcentaje de aciertos.
    
        Nivel máximo: Porcentaje>=90%.
    
        Nivel medio: Porcentaje>=75% y <90%.
    
        Nivel regular: Porcentaje>=50% y <75%.
    
        Fuera de nivel: Porcentaje<50%.

--- Programa_Secuencial/Program.cs
+++ Programa_Secuencial/Program.cs
@@ -1,62 +1,84 @@
-﻿int option;
-Console.WriteLine("Ingrese la opcion que deseas visualizar:");
-Console.ForegroundColor = ConsoleColor.Green;
-Console.WriteLine("1. Realizar la carga de dos números enteros por teclado e imprimir su suma y su producto.");
-Console.WriteLine("2. Se debe desarrollar un programa que pida el ingreso del precio de un artículo y la cantidad que lleva el cliente. Mostrar lo que debe abonar el comprador.");
-Console.WriteLine("3. Realizar un programa que lea cuatro valores numéricos e informar su suma y promedio.");
-option = int.Parse(Console.ReadLine());
+﻿//1.Diseñar un programa que solicite al usuario 
+//dos números y, si el primer número es más
+// grande, calcule y muestre su suma y resta.
+//  De lo contrario, calculará y mostrará su 
+ // producto y cociente.
 
-Console.ForegroundColor = ConsoleColor.White;
-switch(option){
-    case 1:
-//1. Realizar la carga de dos numeros enteros por teclado e imprimir su suma y su producto.
+Console.WriteLine("Ingrese el Primer Numero:");
+double numero1 = Convert.ToDouble(Console.ReadLine());
 
-Console.WriteLine("Ingrese el primer numero:");
-int numero5 = int.Parse(Console.ReadLine());
-Console.WriteLine("Ingrese el segundo numero;");
-int numero6 = int.Parse(Console.ReadLine());
-int suma = numero5 + numero6;
-int producto = numero5 * numero6;
+Console.WriteLine("Ingrese el segundo Numero: ");
+double numero2 = Convert.ToDouble(Console.ReadLine());
 
-Console.WriteLine("La suma de los dos numeros ingresados son: " + suma + " Su producto es: " +producto);
-break;
-
-case 2:
-
-//2. Se debe desarrollar un programa que pida el ingreso del precio de un articulo y la cantidad que lleva el cliente. mostrar lo que debe abonar el comprador.
-Console.WriteLine("Ingrese el Precio del articulo: ");
-decimal precio = Convert.ToDecimal(Console.ReadLine());
-Console.WriteLine("Ingese la Cantidad:");
-int cantidad = int.Parse(Console.ReadLine());
-
-decimal Deuda = Convert.ToDecimal(precio*cantidad);
-
-Console.WriteLine("El total a pagar es: " +Deuda);
-
-break;
-case 3:
-//3. Realizar un programa que lea cuatro valores numericos e informar su suma y promedio.
-
-Console.WriteLine("Ingrese el primer numero");
-int numero1 = int.Parse(Console.ReadLine());
-
-Console.WriteLine("Ingrese el segundo numero");
-int numero2 = int.Parse(Console.ReadLine());
-
-Console.WriteLine("Ingrese el tercer numero");
-int numero3 = int.Parse(Console.ReadLine());
-
-Console.WriteLine("Ingrese el cuarto numero");
-int numero4 = int.Parse(Console.ReadLine());
-
-int sumatoria = numero1 + numero2 + numero3 + numero4;
-int promedio = sumatoria / 4;
-
-Console.WriteLine("La Sumatoria de los cuatros numeros ingresados son:" +sumatoria);
-Console.WriteLine("El Promedio es:" +promedio);
-break;
-
-default:
- Console.WriteLine("Opcion no valida.");
- break;
+if (numero1 > numero2){
+    double suma = numero1 + numero2;
+    double resta = numero1 - numero2;
+    Console.WriteLine($"suma: {suma}");
+    Console.WriteLine($"resta: {resta}");
+}else{
+    double producto = numero1 * numero2;
+    double cociente = numero1 / numero2;
+    Console.WriteLine($"Producto: {producto}");
+    Console.WriteLine($"Cociente: {cociente}");
 }
+Console.ReadKey();
+
+// 2) Leer tres calificaciones y calcular el promedio. Si el promedio es igual o superior a siete, imprimir un mensaje indicando que el alumno ha aprobado.
+
+Console.WriteLine("Ingrese la Primera Califiacion");
+double Califiacion = Convert.ToDouble(Console.ReadLine());
+
+
+Console.WriteLine("Ingrese la Segunda Califiacion");
+double Califiacion2 = Convert.ToDouble(Console.ReadLine());
+
+
+Console.WriteLine("Ingrese la Tercera Califiacion");
+double Califiacion3 = Convert.ToDouble(Console.ReadLine());
+
+double promedio = (Califiacion + Califiacion2 + Califiacion3) / 3;
+Console.WriteLine($"El promedio es: {promedio}");
+
+if (promedio >= 7){
+    Console.WriteLine("El Alumno Ha aprobado");
+}else{
+    Console.WriteLine("El alumno no ha aprobado");
+}
+Console.ReadKey();
+
+
+// 3) Desarrollar un programa que calcule el nivel de un postulante a partir de los resultados de un test. El programa recibirá como entrada el número total de preguntas y las respuestas correctas, y devolverá un nivel según el porcentaje de aciertos.
+
+//     Nivel máximo: Porcentaje>=90%.
+
+//     Nivel medio: Porcentaje>=75% y <90%.
+
+//     Nivel regular: Porcentaje>=50% y <75%.
+
+//     Fuera de nivel: Porcentaje<50%.
+
+Console.WriteLine("Ingrese el numero total de preguntas:");
+int totalpreguntas = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ingrese el número de respuestas correctas:");
+            int respuestasCorrectas = Convert.ToInt32(Console.ReadLine());
+
+            double porcentajeAciertos = (respuestasCorrectas * 100.0) / totalpreguntas;
+
+            Console.WriteLine($"Porcentaje de aciertos: {porcentajeAciertos}%");
+if (porcentajeAciertos >= 90)
+            {
+                Console.WriteLine("Nivel máximo");
+            }
+            else if (porcentajeAciertos >= 75)
+            {
+                Console.WriteLine("Nivel medio");
+            }
+            else if (porcentajeAciertos >= 50)
+            {
+                Console.WriteLine("Nivel regular");
+            }
+            else
+            {
+                Console.WriteLine("Fuera de nivel");
+            }
