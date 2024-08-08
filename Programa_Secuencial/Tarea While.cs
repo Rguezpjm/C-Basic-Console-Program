e0724c0c24565583a423c40b9cba5284b7bb22cb
Rguezpjm <84132936+Rguezpjm@users.noreply.github.com>
2024-08-07 23:17:32 -0400

    Add files via upload
     1) Confeccionar un programa que, a partir de una secuencia de 10 números enteros, calcule y muestre la suma y el promedio aritmético de dichos números.
    
    2) Crear un programa que cuente cuántas piezas de hierro de un lote, dentro de un rango específico de longitud, son aptas para fabricar perfiles.

---
+++ Programa_Secuencial/Tarea While.cs
@@ -0,0 +1,13 @@
+﻿   int contador = 0;
+        int suma = 0;
+
+        while (contador < 10) {
+            Console.Write("Introduce un número entero: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            suma += numero;
+            contador++;
+        }
+
+        double promedio = (double)suma / 10;
+        Console.WriteLine($"La suma de los números es: {suma}");
+        Console.WriteLine($"El promedio aritmético es: {promedio:F2}");
