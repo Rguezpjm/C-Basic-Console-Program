7d8cef7dd9b99194c440b8ab908e728fd3a95e0a
Rguezpjm <84132936+Rguezpjm@users.noreply.github.com>
2024-08-24 05:47:53 -0700

    Add files via upload

---
+++ Programa_Secuencial/Calculo de Triangulo.cs
@@ -0,0 +1,28 @@
+﻿namespace proyecto_Sueldo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       Console.WriteLine("Ingrese la cantidad de Triangulos que quiere Calcular:");
+       int total_triangulo = int.Parse(Console.ReadLine());
+
+       int contador_triangulo = 0;
+
+       for (int i = 1; i <= total_triangulo; i++){
+         Console.WriteLine($"Triangulo {i}:");
+         Console.WriteLine("Ingrese la base: ");
+         double baseTriangulo = double.Parse(Console.ReadLine());
+
+         Console.WriteLine("Ingrese la altura: ");
+         double altura = double.Parse(Console.ReadLine());
+
+         double area = (baseTriangulo * altura) / 2;
+         Console.WriteLine($"Base: {baseTriangulo}, Altura: {altura}, Area: {area}");
+
+         if (area > 12){
+            contador_triangulo++;
+         }
+       }
+    }
+}
