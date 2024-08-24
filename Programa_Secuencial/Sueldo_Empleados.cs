b73f04c04bcaf83f3ab8bb48aa2d3a257fc4bfc6
Rguezpjm <84132936+Rguezpjm@users.noreply.github.com>
2024-08-24 06:32:02 -0700

    Add files via upload

---
+++ Programa_Secuencial/Sueldo_Empleados.cs
@@ -0,0 +1,39 @@
+﻿namespace proyecto_Sueldo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       int numero_empleados;
+       double sueldo;
+       int empleadosentre = 0;
+       int empleadosmasde = 0;
+       double gastoTotal = 0;
+
+       Console.WriteLine("Ingrese el numero de empleados: ");
+       numero_empleados = int.Parse(Console.ReadLine());
+
+       for ( int i = 1; i <= numero_empleados; i++){
+        100
+        Console.WriteLine($"Ingrese el sueldo del empleado {i}: $");
+        sueldo = double.Parse(Console.ReadLine());
+
+        if ( sueldo >= 100 && sueldo <= 300){
+            empleadosentre++;
+        }else if (sueldo > 300){
+            empleadosmasde++;
+        }
+            gastoTotal += sueldo;
+       }
+    
+        Console.WriteLine($"Empleados con sueldo entre $100 y $300: {empleadosentre}" );
+        Console.WriteLine($"Empleados con sueldo mayor a $300: {empleadosmasde}" );
+        Console.WriteLine($"Importe total en sueldos: ${gastoTotal}");
+
+
+        Console.ReadKey();
+
+
+
+    }
+}
