f96b599a9c71e0e27bac12492d4a0f1ff6e4ad21
Rguezpjm <84132936+Rguezpjm@users.noreply.github.com>
2024-08-28 22:05:22 -0400

    Add files via upload

---
+++ Programa_Secuencial/Array #1.cs
@@ -0,0 +1,15 @@
+﻿namespace proyecto_Sueldo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int [] array = new int[20];
+        for (int i = 0; i < array.Length; i++){
+            array[i] = i*5;
+        }
+        foreach (int value in array){
+            Console.WriteLine(value);
+        }
+    }
+}
