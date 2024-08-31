18017b6c60448378c8c09d0ec3ad82df01254a19
Rguezpjm <84132936+Rguezpjm@users.noreply.github.com>
2024-08-31 09:07:39 -0400

    Add files via upload

---
+++ Programa_Secuencial/ARRAY#2.cs
@@ -0,0 +1,20 @@
+﻿namespace proyecto_Sueldo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       Console.WriteLine("Ingrese el primer elemento de la matriz separado por espacios:");
+       int [] primerlemento = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+       Console.WriteLine("Ingrese el segundo elemento de la matriz separado por espacios:");
+       int [] segundoelemento = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+        bool igualdad = primerlemento.SequenceEqual(segundoelemento);    
+        if (igualdad){
+            Console.WriteLine("Las matrices son iguales.");
+        }else{
+            Console.WriteLine("Las matrices no son iguales.");
+        }
+    }
+}
